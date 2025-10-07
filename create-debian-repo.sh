6f8c8e625f3450555a4a6eacb149bca5a53647ff
Markson Hon <markson@fedora.lan>
2025-10-07 17:17:16 +0800

    init debian repo key

--- create-debian-repo.sh
+++ create-debian-repo.sh
@@ -11,7 +11,7 @@ export debian_origin="daeuniverse"
 export debian_label="daeuniverse"
 export debian_suite="stable"
 export debian_codename="goose"
-export debian_architecture="amd64 arm64 i386 riscv64 all"
+export debian_architecture="amd64 arm64 i386 all"
 export debian_components="main"
 export debian_description="A Debian repository for v2rayA, dae and juicity."
 
@@ -23,6 +23,9 @@ Codename: ${debian_codename}
 Architectures: ${debian_architecture}
 Components: ${debian_components}
 Description: ${debian_description}
-SignWith: yes
+SignWith: C27FABADF6B07760
 EOL
 
+for deb_file in ./archive/*.deb; do
+    reprepro -b repo includedeb ${debian_codename} "$deb_file"
+done
