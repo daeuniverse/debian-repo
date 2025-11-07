dd3110072648982c0852f0251f2a410b6269706c
Markson Hon <50002150+MarksonHon@users.noreply.github.com>
2025-11-07 18:14:49 +0800

    Add daed to Debian repo (#2)
    * 1. remove nfpm's example notice 2. add daed
    
    * add more notice in ReadMe
    
    ---------
    
    Co-authored-by: Markson Hon <markson@fedora.lan>

--- README.md
+++ README.md
@@ -13,15 +13,6 @@ sudo apt update
 sudo apt install curl gpg
 ```
 
-#### For APT version lower than 3.0
-
-Add the repository to your sources list:
-
-```sh
-cat <<- EOL | sudo tee /etc/apt/sources.list.d/daeuniverse.list
-deb [signed-by=/usr/share/keyrings/daeuniverse-archive-goose.gpg] https://daeuniverse.pages.dev goose honk 
-EOL
-```
 #### For APT version 3.0 or higher
 
 Add the repository to your sources config:
@@ -32,8 +23,17 @@ Types: deb
 URIs: https://daeuniverse.pages.dev
 Suites: goose
 Components: honk
-Architectures: amd64 arm64 i386 riscv64
 Signed-By: /usr/share/keyrings/daeuniverse-archive-goose.gpg
+EOL
+```
+
+#### For APT version lower than 3.0
+
+Add the repository to your sources list:
+
+```sh
+cat <<- EOL | sudo tee /etc/apt/sources.list.d/daeuniverse.list
+deb [signed-by=/usr/share/keyrings/daeuniverse-archive-goose.gpg] https://daeuniverse.pages.dev goose honk 
 EOL
 ```
 
@@ -54,4 +54,38 @@ Install the desired package, for example, v2rayA:
 
 ```sh
 sudo apt install v2raya
+```
+
+## Available packages
+
+see: <https://github.com/daeuniverse/debian-repo/tree/main/nfpm>
+
+## How to edit Systemd Service
+
+If you want to edit the systemd service file, you can just run(for example, for `daed` service):
+
+```sh
+sudo systemctl edit --full daed.service
+```
+
+New file will be placed in `/etc/systemd/system/daed.service`, instead of in `/lib/systemd/system/daed.service`, and new file will not be overwritten when package is updated.
+
+## Let v2rayA use v2ray as its core
+
+Defaultly v2rayA uses xray as its core. To let v2rayA use v2ray as its core, edit the v2raya service file:
+
+```sh
+sudo systemctl edit --full v2raya.service
+```
+then add environment variable `V2RAYA_V2RAY_PATH` in the `[Service]` section:
+
+```ini
+[Service]
+Environment="V2RAYA_V2RAY_PATH=/usr/bin/v2ray"
+```
+
+Don't remove other existing lines and make sure you have v2ray installed in your system, then  restart v2rayA:
+
+```sh
+sudo systemctl restart v2raya.service
 ```
---
+++ downloader/daed.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+daed_temp_file="$(mktemp /tmp/daed.XXXXXX)"
+if ! curl -s "https://api.github.com/repos/daeuniverse/daed/releases/latest" -o "$daed_temp_file"; then
+    echo "Error: Cannot get latest version of dae!"
+    exit 1
+fi
+daed_remote_version=$(grep tag_name "$daed_temp_file" | awk -F "tag_name" '{printf $2}' | awk -F "," '{printf $1}' | awk -F '"' '{printf $3}')
+daed_url_amd64="https://github.com/daeuniverse/daed/releases/download/${daed_remote_version}/daed-linux-x86_64.zip"
+daed_url_arm64="https://github.com/daeuniverse/daed/releases/download/${daed_remote_version}/daed-linux-arm64.zip"
+daed_url_i386="https://github.com/daeuniverse/daed/releases/download/${daed_remote_version}/daed-linux-x86_32.zip"
+daed_url_riscv64="https://github.com/daeuniverse/daed/releases/download/${daed_remote_version}/daed-linux-riscv64.zip"
+rm -f "$daed_temp_file"
+
+daed_temp_dir="$(mktemp -d /tmp/dae.XXXXXX)"
+curl -L "$daed_url_amd64" -o "$daed_temp_dir/daed_amd64_${daed_remote_version}.zip"
+curl -L "$daed_url_arm64" -o "$daed_temp_dir/daed_arm64_${daed_remote_version}.zip"
+curl -L "$daed_url_i386" -o "$daed_temp_dir/daed_i386_${daed_remote_version}.zip"
+curl -L "$daed_url_riscv64" -o "$daed_temp_dir/daed_riscv64_${daed_remote_version}.zip"
+unzip "$daed_temp_dir/daed_amd64_${daed_remote_version}.zip" daed-linux-x86_64 -d ./ && mv ./daed-linux-x86_64 ./daed_amd64_${daed_remote_version} && chmod +x ./daed_amd64_${daed_remote_version}
+unzip "$daed_temp_dir/daed_arm64_${daed_remote_version}.zip" daed-linux-arm64 -d ./ && mv ./daed-linux-arm64 ./daed_arm64_${daed_remote_version} && chmod +x ./daed_arm64_${daed_remote_version}
+unzip "$daed_temp_dir/daed_i386_${daed_remote_version}.zip" daed-linux-x86_32 -d ./ && mv ./daed-linux-x86_32 ./daed_i386_${daed_remote_version} && chmod +x ./daed_i386_${daed_remote_version}
+unzip "$daed_temp_dir/daed_riscv64_${daed_remote_version}.zip" daed-linux-riscv64 -d ./ && mv ./daed-linux-riscv64 ./daed_riscv64_${daed_remote_version} && chmod +x ./daed_riscv64_${daed_remote_version}
+rm -rf "$daed_temp_dir"
+echo ${daed_remote_version#v} > daed_version.txt
