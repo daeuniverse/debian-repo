caf081b701bfc9b11e85eec4e5d550ebf60a1ac4
Markson Hon <markson@fedora.lan>
2025-12-03 18:04:01 +0800

    daed: fix build

--- downloader/daed.sh
+++ downloader/daed.sh
@@ -17,9 +17,9 @@ curl -L "$daed_url_amd64" -o "$daed_temp_dir/daed_amd64_${daed_remote_version}.z
 curl -L "$daed_url_arm64" -o "$daed_temp_dir/daed_arm64_${daed_remote_version}.zip"
 curl -L "$daed_url_i386" -o "$daed_temp_dir/daed_i386_${daed_remote_version}.zip"
 curl -L "$daed_url_riscv64" -o "$daed_temp_dir/daed_riscv64_${daed_remote_version}.zip"
-unzip "$daed_temp_dir/daed_amd64_${daed_remote_version}.zip" daed-linux-x86_64 -d ./ && mv ./daed-linux-x86_64 ./daed_amd64_${daed_remote_version} && chmod +x ./daed_amd64_${daed_remote_version}
-unzip "$daed_temp_dir/daed_arm64_${daed_remote_version}.zip" daed-linux-arm64 -d ./ && mv ./daed-linux-arm64 ./daed_arm64_${daed_remote_version} && chmod +x ./daed_arm64_${daed_remote_version}
-unzip "$daed_temp_dir/daed_i386_${daed_remote_version}.zip" daed-linux-x86_32 -d ./ && mv ./daed-linux-x86_32 ./daed_i386_${daed_remote_version} && chmod +x ./daed_i386_${daed_remote_version}
-unzip "$daed_temp_dir/daed_riscv64_${daed_remote_version}.zip" daed-linux-riscv64 -d ./ && mv ./daed-linux-riscv64 ./daed_riscv64_${daed_remote_version} && chmod +x ./daed_riscv64_${daed_remote_version}
+unzip "$daed_temp_dir/daed_amd64_${daed_remote_version}.zip" "daed-linux-x86_64/daed-linux-x86_64" -d ./ && mv ./daed-linux-x86_64/daed-linux-x86_64 ./daed_amd64_${daed_remote_version} && chmod +x ./daed_amd64_${daed_remote_version}
+unzip "$daed_temp_dir/daed_arm64_${daed_remote_version}.zip" "daed-linux-arm64/daed-linux-arm64" -d ./ && mv ./daed-linux-arm64/daed-linux-arm64 ./daed_arm64_${daed_remote_version} && chmod +x ./daed_arm64_${daed_remote_version}
+unzip "$daed_temp_dir/daed_i386_${daed_remote_version}.zip" "daed-linux-x86_32/daed-linux-x86_32" -d ./ && mv ./daed-linux-x86_32/daed-linux-x86_32 ./daed_i386_${daed_remote_version} && chmod +x ./daed_i386_${daed_remote_version}
+unzip "$daed_temp_dir/daed_riscv64_${daed_remote_version}.zip" "daed-linux-riscv64/daed-linux-riscv64" -d ./ && mv ./daed-linux-riscv64/daed-linux-riscv64 ./daed_riscv64_${daed_remote_version} && chmod +x ./daed_riscv64_${daed_remote_version}
 rm -rf "$daed_temp_dir"
 echo ${daed_remote_version#v} > daed_version.txt
